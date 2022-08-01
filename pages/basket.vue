6dd71eaaef14defb105acdaf922b43c6897d90f4
Aaliya Farhin <aaliya.farhin@corp.ola.com>
2022-08-01 17:31:15 +0530

    structure crerated

---
+++ pages/basket.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="m-8">
+     <h6>Please choose items to your bucket</h6>
+     <div class="flex flex-row ">
+      <div class="basis-1/4 bg-red-400">
+        <div>Apple</div>
+        <div>{{counter}}</div>
+        <div><button v-on:click="add" >+</button><button v-on:click="remove">-</button></div>
+
+      </div>
+      <div class="basis-1/4 bg-orange-500">
+        <div>Orange</div>
+        <div>{{orangecounter}}</div>
+        <div><button v-on:click="add1">+</button><button v-on:click="remove1">-</button></div>
+
+      </div>
+      <div class="basis-1/4 bg-blue-200">
+        <div >Grape</div>
+        <div>{{grapecounter}}</div>
+        <div><button v-on:click="add2" class="rounded-full">+</button><button v-on:click="remove2">-</button></div>
+
+      </div>
+     </div>
+    </div>
+    <div class="m-8">
+      <div>
+        <h4>Basket stack</h4>
+      </div>
+      <div>{{counter}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { remove } from '@vue/shared';
+
+export default {
+   data(){
+    return{
+      counter:0,
+      orangecounter:0,
+      grapecounter:0,
+      basket:[]
+
+    }
+  },
+  methods:{
+    add(){
+      this.counter = this.counter === 10 ? 10 : this.counter + 1;
+      this.basket=this.counter
+    },
+    remove(){
+     this.counter = this.counter === 1 ? 1 : this.counter - 1;
+    },
+    add1(){
+      this.orangecounter = this.orangecounter === 10 ? 10 : this.orangecounter + 1;
+    },
+    remove1(){
+     this.orangecounter = this.orangecounter === 1 ? 1 : this.orangecounter - 1;
+    },
+    add2(){
+      this.grapecounter = this.grapecounter === 10 ? 10 : this.grapecounter + 1;
+    },
+    remove2(){
+     this.grapecounter = this.grapecounter === 1 ? 1 : this.grapecounter - 1;
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <form>
+        <h1>Login</h1>
+        <div><input type="text" placeholder="username" v-model="name"/>
+        </div>
+        <div><input type="text" placeholder="password" v-model="password"/></div>
+        <div><button v-on:click="login() " type="submit" >Submit</button><NuxtLink to="/basket"></NuxtLink></div>
+        
+        
+    </form>
+  </div>
 </template>
 
 <script>
+import axios from "axios"
+import { NuxtLink } from '~~/.nuxt/components'
 export default {
-
+data(){
+    return{
+     name:'',
+     password:''
+    }
+},
+methods:{
+     async login(){
+        let result = await axios.get(
+            `http://localhost:3000/users?name=${this.name}&password=${this.password}`
+        )
+        if(result.status==200 && result.length.data>0){
+            localStorage.setItem("user-info",JSON.stringify(result.data[0]))
+            
+        }
+    }
+}
 }
 </script>
 
